910116b907af34485974a973492a3dda4e73f4b8
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-01-28 22:38:24 -0600

    username

--- mainmonoamie.py
+++ mainmonoamie.py
@@ -3,4 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    
     return 'Bienvenido a Monoamie'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'Hola %s' % username
