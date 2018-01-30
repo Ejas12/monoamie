e4974ae784733d91169c30243daddbb9731abab5
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-01-30 10:46:30 -0600

    bad html

--- forms.py
+++ forms.py
@@ -6,5 +6,5 @@ from wtforms.validators import DataRequired
 class Loginform(FlaskForm):
     username = StringField('Usuario:', validators=[DataRequired()])
     pasword = PasswordField('Password:', validators=[DataRequired()])
-    submit = SubmitField ('Entrat')
+    submit = SubmitField ('Entrar')
     
