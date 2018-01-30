6f7df1b8f25c8f273f578ff123c1cd313c4db38e
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-01-30 13:06:08 -0600

    log in con submit

--- mainmonoamie.py
+++ mainmonoamie.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
 from config import Config
 from forms import Loginform
 app = Flask(__name__)
@@ -17,7 +17,10 @@ def show_user_profile(username):
     # show the user profile for that user
     return 'Hola %s' % username
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = Loginform()
+    if form.validate_on_submit():
+        flash('Usuario logueado {},'.format(form.username.data))
+        return redirect('/')
     return render_template('Loginform.html', title='Sign In', form=form)
