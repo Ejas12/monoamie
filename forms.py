df89e9b0cdbd90f3ae498008f36d37ffb6c68eeb
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-01-30 10:15:33 -0600

    form and login

---
+++ forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class Loginform(FlaskForm):
+    username = StringField('Usuario:', validators=[DataRequired()])
+    pasword = PasswordField('Password:', validators=[DataRequired()])
+    submit = SubmitField ('Entrat')
+    
--- mainmonoamie.py
+++ mainmonoamie.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from config import Config
+from mainmonoamie import app
+from mainmonoamie.forms import LoginForm
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,3 +16,8 @@ if __name__ == '__main__':
 def show_user_profile(username):
     # show the user profile for that user
     return 'Hola %s' % username
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
