619a24d763377d05794ffdd8f5b0323ba7ab0da7
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-01-30 21:00:59 -0600

    unicode fix

--- mainmonoamie.py
+++ mainmonoamie.py
@@ -32,7 +32,7 @@ def login():
 @app.route('/testreport')
 def testreport():
 
-    connectionobj = MySQLdb.connect(host='172.31.25.244', user='root', passwd='Anonos123', db='liftinghands')
+    connectionobj = MySQLdb.connect(host='172.31.25.244', user='root', passwd='Anonos123', db='liftinghands', charset='utf8', use_unicode=True)
     DBcursor = connectionobj.cursor()
     DBcursor.execute("SELECT liftinghands.students.first_name as 'Nombre',liftinghands.students.last_name as 'Apellido', liftinghands.students.CUSTOM_10 as 'Segundo Apellido', liftinghands.students.phone as 'Telefono directo', liftinghands.students.CUSTOM_11 as 'Encargado 1', liftinghands.students.CUSTOM_12 as 'Telefono Encargado 1', liftinghands.students.CUSTOM_13 as 'Encargado 2', liftinghands.students.CUSTOM_14 as 'Telefono Encargado 2', liftinghands.students.CUSTOM_15 as 'Direccion' FROM liftinghands.students;")
     listaninos = DBcursor.fetchall()
