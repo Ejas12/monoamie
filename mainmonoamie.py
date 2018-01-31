9557ac143a51b04c0fbd27a0c7c8c044d5d1e582
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-01-30 20:54:08 -0600

    proper report

--- mainmonoamie.py
+++ mainmonoamie.py
@@ -34,6 +34,6 @@ def testreport():
 
     connectionobj = MySQLdb.connect(host='172.31.25.244', user='root', passwd='Anonos123', db='liftinghands')
     DBcursor = connectionobj.cursor()
-    DBcursor.execute("SELECT * FROM liftinghands.students;")
+    DBcursor.execute("SELECT liftinghands.students.first_name as 'Nombre',liftinghands.students.last_name as 'Apellido', liftinghands.students.CUSTOM_10 as 'Segundo Apellido', liftinghands.students.phone as 'Telefono directo', liftinghands.students.CUSTOM_11 as 'Encargado 1', liftinghands.students.CUSTOM_12 as 'Telefono Encargado 1', liftinghands.students.CUSTOM_13 as 'Encargado 2', liftinghands.students.CUSTOM_14 as 'Telefono Encargado 2', liftinghands.students.CUSTOM_15 as 'Direccion' FROM liftinghands.students;")
     listaninos = DBcursor.fetchall()
     return render_template('table.html', title='Reporte Test', data=listaninos)
