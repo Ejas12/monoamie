217aeb48c6b99c227c02ac5a39a389e670a0ff4f
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-01-31 18:02:29 -0600

    listas de clases

--- mainmonoamie.py
+++ mainmonoamie.py
@@ -13,11 +13,7 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-    
-@app.route('/user/<username>')
-def show_user_profile(username):
-    # show the user profile for that user
+
     return 'Hola %s' % username
 
 @app.route('/login', methods=['GET', 'POST'])
