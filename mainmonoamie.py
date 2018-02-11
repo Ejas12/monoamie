cba620e4978ec020789ab855e1ec456a8507ddb8
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-02-11 16:13:31 -0600

    agregado lista con cumples

--- mainmonoamie.py
+++ mainmonoamie.py
@@ -104,3 +104,35 @@ group by liftinghands.students.student_id
 order by liftinghands.students.student_id;""")
     listaninos = DBcursor.fetchall()
     return render_template('tablematriculados.html', title='Reporte Test', data=listaninos)
+
+
+@app.route('/Listaconcumples')
+def Listaconcumples():
+
+    connectionobj = MySQLdb.connect(host='172.31.25.244', user='root', passwd='Anonos123', db='liftinghands', charset='utf8', use_unicode=True)
+    DBcursor = connectionobj.cursor()
+    DBcursor.execute("""
+SELECT liftinghands.students.first_name as 'Nombre',
+liftinghands.students.last_name as 'Apellido',
+liftinghands.students.CUSTOM_10 as 'Segundo Apellido',
+liftinghands.students.phone as 'Telefono directo',
+liftinghands.students.birthdate as 'cumpleanos',
+liftinghands.enroll_grade.title as 'Grado',
+liftinghands.students.CUSTOM_11 as 'Encargado 1',
+liftinghands.students.CUSTOM_12 as 'Telefono Encargado 1',
+liftinghands.students.CUSTOM_13 as 'Encargado 2',
+liftinghands.students.CUSTOM_14 as 'Telefono Encargado 2',
+liftinghands.students.CUSTOM_15 as 'Direccion'
+
+
+FROM liftinghands.students
+
+left  JOIN liftinghands.schedule ON liftinghands.schedule.student_id=liftinghands.students.student_id
+left join liftinghands.enroll_grade on liftinghands.enroll_grade.student_id=liftinghands.students.student_id
+where liftinghands.schedule.course_id is not  null and  liftinghands.students.first_name !='Deleted'
+group by liftinghands.students.student_id
+order by liftinghands.students.student_id;
+""")
+
+    listaninos = DBcursor.fetchall()
+    return render_template('tablematriculadosconcumples.html', title='Reporte Test', data=listaninos)
