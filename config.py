12c2b048bb6ec15c5a19cd577660955ca2a931d5
Esteban Artavia <Esteban_Artavia@mckinsey.com>
2018-09-23 20:40:22 -0600

    sql alchemy conf

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ASOIF-Gameon8991'
-
-SQLALCHEMY_DATABASE_URI = 'mysql://liftingmonoami:LiftingAnonos2018@localhost/tablahorarios'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql://liftingmonoami:LiftingAnonos2018@localhost/tablahorarios'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
